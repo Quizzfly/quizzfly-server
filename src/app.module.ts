4d44a7e703a5fd1eec267a5906c7486ca2f685c1
LeTatTuan <letattuan04052003@gmail.com>
2024-09-26 05:56:00 +0700

    fix: resolve run docker without dist directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import generateModulesSet from '@/modules-set';
 import { AuthModule } from '@modules//auth/auth.module';
 import { HealthModule } from '@modules/health/health.module';
 import { HomeModule } from '@modules/home/home.module';
 import { Module } from '@nestjs/common';
+import generateModulesSet from '@shared/modules-set';
 
 const modulesGenerate = generateModulesSet();
 @Module({
--- src/shared/modules-set.ts
+++ src/shared/modules-set.ts
@@ -57,7 +57,7 @@ function generateModulesSet() {
           infer: true,
         }),
         loaderOptions: {
-          path: path.join(__dirname, '/i18n/'),
+          path: path.join(__dirname, '/../i18n/'),
           watch: isLocal,
         },
         typesOutputPath: path.join(
