9a968debe2c4cf683597e2b45b1ff8a541016422
LeTatTuan <letattuan04052003@gmail.com>
2024-10-03 07:40:15 +0700

    feat: implement repository pattern for service user and session

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { AuthModule } from '@modules//auth/auth.module';
 import { HealthModule } from '@modules/health/health.module';
 import { HomeModule } from '@modules/home/home.module';
+import { SessionModule } from '@modules/session/session.module';
+import { UserModule } from '@modules/user/user.module';
 import { Module } from '@nestjs/common';
 import generateModulesSet from '@shared/modules-set';
+import { SharedModule } from '@shared/shared.module';
 
 const modulesGenerate = generateModulesSet();
+
 @Module({
-  imports: [...modulesGenerate, HealthModule, AuthModule, HomeModule],
+  imports: [
+    ...modulesGenerate,
+    HealthModule,
+    AuthModule,
+    HomeModule,
+    UserModule,
+    SessionModule,
+    SharedModule,
+  ],
 })
 export class AppModule {}
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -20,11 +20,40 @@ export class AuthController {
 
   @ApiPublic({
     type: LoginResDto,
-    summary: 'Sign in',
+    summary: 'Sign in for admin',
+  })
+  @Post('admin/login')
+  async signInAdmin(@Body() userLogin: LoginReqDto): Promise<LoginResDto> {
+    return this.authService.signIn(userLogin);
+  }
+
+  @ApiAuth({
+    summary: 'Logout for admin',
+    errorResponses: [400, 401, 403, 500],
+  })
+  @Post('admin/logout')
+  async logoutAdmin(
+    @CurrentUser('sessionId') sessionId: string,
+  ): Promise<void> {
+    await this.authService.logout(sessionId);
+  }
+
+  @ApiPublic({
+    type: RefreshResDto,
+    summary: 'Refresh token for admin',
+  })
+  @Post('admin/refresh')
+  async refreshAdmin(@Body() dto: RefreshReqDto): Promise<RefreshResDto> {
+    return this.authService.refreshToken(dto);
+  }
+
+  @ApiPublic({
+    type: LoginResDto,
+    summary: 'Sign in for user',
   })
   @Post('login')
   async signIn(@Body() userLogin: LoginReqDto): Promise<LoginResDto> {
-    return await this.authService.signIn(userLogin);
+    return this.authService.signIn(userLogin);
   }
 
   @ApiPublic({
@@ -33,7 +62,7 @@ export class AuthController {
   })
   @Post('register')
   async register(@Body() dto: RegisterReqDto): Promise<RegisterResDto> {
-    return await this.authService.register(dto);
+    return this.authService.register(dto);
   }
 
   @ApiAuth({
@@ -51,7 +80,7 @@ export class AuthController {
   })
   @Post('refresh')
   async refresh(@Body() dto: RefreshReqDto): Promise<RefreshResDto> {
-    return await this.authService.refreshToken(dto);
+    return this.authService.refreshToken(dto);
   }
 
   @ApiPublic()
