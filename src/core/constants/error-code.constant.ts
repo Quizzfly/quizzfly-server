d05f34c002d1a2c0d451dd83a3a9f45b9f7831ff
phduchuy <phanhoangduchuyy@gmail.com>
2024-10-09 21:37:27 +0700

    fix: fix review

--- src/core/constants/error-code.constant.ts
+++ src/core/constants/error-code.constant.ts
@@ -11,6 +11,7 @@ export enum ErrorCode {
   E001 = 'user.error.username_or_email_exists',
   E002 = 'user.error.not_found',
   E003 = 'user.error.email_exists',
+  E004 = 'quizzfly.error.not_found',
 
   // Authentication Errors
   A001 = 'auth.error.invalid_credentials',
@@ -21,4 +22,5 @@ export enum ErrorCode {
   A006 = 'auth.error.refresh_token_invalid',
   A007 = 'auth.error.account_locked',
   A008 = 'auth.error.account_disabled',
+  A009 = 'auth.error.forbidden',
 }
