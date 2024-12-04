2f80317135c5d4e620a5020c76a060955e08b4de
phduchuy <phanhoangduchuyy@gmail.com>
2024-12-04 13:38:53 +0700

    feat: realtime group

--- src/modules/group/repository/post.repository.ts
+++ src/modules/group/repository/post.repository.ts
@@ -78,4 +78,65 @@ export class PostRepository extends Repository<PostEntity> {
 
     return convertSnakeToCamel(await query.getRawMany());
   }
+
+  async getDetailPost(postId: Uuid) {
+    const query = this.createQueryBuilder('post')
+      .leftJoinAndSelect('post.quizzfly', 'quizzfly')
+      .leftJoinAndSelect('post.member', 'member')
+      .leftJoinAndSelect('member.userInfo', 'memberInfo')
+      .select([
+        'post.id as id',
+        'post.createdAt as created_at',
+        'post.updatedAt as updated_at',
+        'post.type as type',
+        'post.content as content',
+        'post.files as files',
+        'quizzfly.id as quizzfly_id',
+        'quizzfly.title as title',
+        'quizzfly.description as description',
+        'quizzfly.coverImage as cover_image',
+        'quizzfly.theme as theme',
+        'quizzfly.isPublic as is_public',
+        'quizzfly.quizzflyStatus as quizzfly_status',
+        'memberInfo.username as username',
+        'memberInfo.avatar as avatar',
+        'memberInfo.name as name',
+        'member.id as member_id',
+      ])
+      .addSelect(
+        (subQuery) =>
+          subQuery
+            .select('CAST(COUNT(*) AS INTEGER)')
+            .from('react_post', 'react')
+            .where('react.postId = post.id')
+            .andWhere('react.deletedAt IS NULL'),
+        'reactCount',
+      )
+      .addSelect(
+        (subQuery) =>
+          subQuery
+            .select('CAST(COUNT(*) AS INTEGER)')
+            .from('comment_post', 'comment')
+            .where('comment.postId = post.id')
+            .andWhere('comment.deletedAt IS NULL'),
+        'commentCount',
+      )
+      .addSelect(
+        (subQuery) =>
+          subQuery
+            .select(
+              'CASE WHEN COUNT(*) > 0 THEN true ELSE false END',
+              'isLiked',
+            )
+            .from('react_post', 'react')
+            .where('react.postId = post.id')
+            .andWhere('react.memberId = member.id')
+            .andWhere('react.deletedAt IS NULL'),
+        'isLiked',
+      )
+      .where('post.id = :postId', { postId })
+      .andWhere('post.deletedAt IS NULL');
+
+    return convertSnakeToCamel(await query.getRawOne());
+  }
 }
--- src/modules/group/socket/group-socket.gateway.ts
+++ src/modules/group/socket/group-socket.gateway.ts
@@ -1,20 +1,16 @@
-import { EventService } from '@core/events/event.service';
-import { WebsocketExceptionFilter } from '@core/filters/websocket-exception.filter';
+import { Uuid } from '@common/types/common.type';
 import { convertCamelToSnake } from '@core/helpers';
-import { WsValidationPipe } from '@core/pipes/ws-validation.pipe';
-import { RegisterGroupReqDto } from '@modules/group/socket/payload/request/register-group.req.dto';
+import { MemberInGroupRepository } from '@modules/group/repository/member-in-group.repository';
 import { Logger, UseFilters } from '@nestjs/common';
 import {
-  ConnectedSocket,
-  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
-  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { WebsocketExceptionFilter } from '@core/filters/websocket-exception.filter';
 
 @UseFilters(WebsocketExceptionFilter)
 @WebSocketGateway({
@@ -24,34 +20,36 @@ import { Server, Socket } from 'socket.io';
   namespace: '/groups',
 })
 export class GroupSocketGateway
-  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
-{
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   private readonly logger = new Logger(GroupSocketGateway.name);
 
-  constructor(private readonly eventService: EventService) {}
+  constructor(
+    private readonly memberInGroupRepository: MemberInGroupRepository,
+  ) {
+  }
 
   afterInit(server: Server) {
     this.logger.log(server);
   }
 
-  handleConnection(client: Socket, ...args: any[]) {
+  async handleConnection(client: Socket, ...args: any[]) {
     this.logger.log(`Connected ${client.id} group`);
+    const userId = client.handshake.query.user_id as Uuid;
+    const memberInGroups = await this.memberInGroupRepository.findBy({
+      memberId: userId,
+    });
+
+    memberInGroups.forEach((memberInGroups) => {
+      client.join(memberInGroups.groupId);
+      this.logger.log(
+        `Client ${client.id} registered to group ${memberInGroups.groupId}`,
+      );
+    });
   }
 
   handleDisconnect(client: Socket) {
     this.logger.log(`Disconnected: ${client.id}`);
-  }
-
-  @SubscribeMessage('registerGroup')
-  handleRegisterGroup(
-    @ConnectedSocket() client: Socket,
-    @MessageBody(new WsValidationPipe()) data: RegisterGroupReqDto,
-  ) {
-    data.groupIds.forEach((id) => {
-      client.join(id);
-      this.logger.log(`Client ${client.id} registered to group ${id}`);
-    });
   }
 
   sendToGroup<T>(groupId: string, event: string, data: T): void {
