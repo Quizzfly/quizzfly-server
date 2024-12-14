5ada6e8b5f48086352d12d683c28968ccdbb7088
phduchuy <phanhoangduchuyy@gmail.com>
2024-12-14 22:54:02 +0700

    update: add description column to notification table

--- src/modules/notification/service/push-notification.service.ts
+++ src/modules/notification/service/push-notification.service.ts
@@ -1,8 +1,10 @@
 import { NotificationService } from '@/modules/notification/service/notification.service';
 import { Uuid } from '@common/types/common.type';
 import { CreateNotificationDto } from '@modules/notification/dto/request/create-notification.dto';
+import { NotificationResDto } from '@modules/notification/dto/response/notification.res.dto';
 import { NotificationSocketGateway } from '@modules/notification/socket/notification-socket.gateway';
 import { Injectable } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class PushNotificationService {
@@ -14,18 +16,32 @@ export class PushNotificationService {
   async pushNotificationToUser(data: CreateNotificationDto, userId: Uuid) {
     const notification =
       await this.notificationService.createNotification(data);
-    this.notificationSocketGateway.pushNotificationToUser(userId, notification);
+
+    const savedNotification = await this.notificationService.findById(
+      notification.id,
+    );
+    this.notificationSocketGateway.pushNotificationToUser(
+      userId,
+      plainToInstance(NotificationResDto, savedNotification, {
+        excludeExtraneousValues: true,
+      }),
+    );
   }
 
   async pushNotifcationToUsers(items: CreateNotificationDto[]) {
     const notifications =
       await this.notificationService.createNotifications(items);
 
-    notifications.forEach((notification) => {
+    for (const notification of notifications) {
+      const savedNotification = await this.notificationService.findById(
+        notification.id,
+      );
       this.notificationSocketGateway.pushNotificationToUser(
         notification.receiverId,
-        notification,
+        plainToInstance(NotificationResDto, savedNotification, {
+          excludeExtraneousValues: true,
+        }),
       );
-    });
+    }
   }
 }
