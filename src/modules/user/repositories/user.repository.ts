910acf5c9ba9b4229011a60adaa33fb0b88e3d82
phduchuy <phanhoangduchuyy@gmail.com>
2024-12-19 21:28:43 +0700

    feat: api admin user

--- src/modules/user/repositories/user.repository.ts
+++ src/modules/user/repositories/user.repository.ts
@@ -1,4 +1,7 @@
+import { PageOptionsDto } from '@common/dto/offset-pagination/page-options.dto';
 import { Uuid } from '@common/types/common.type';
+import { ROLE } from '@core/constants/entity.enum';
+import { convertSnakeToCamel } from '@core/helpers';
 import { UserEntity } from '@modules/user/entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
@@ -22,5 +25,28 @@ export class UserRepository extends Repository<UserEntity> {
       .leftJoinAndSelect('user.userInfo', 'user_info')
       .where('user.id = :id', { id: userId })
       .getOne();
+  }
+
+  async getListUser(filterOptions: PageOptionsDto) {
+    const skip = filterOptions.page
+      ? (filterOptions.page - 1) * filterOptions.limit
+      : 0;
+
+    const query = this.createQueryBuilder('user')
+      .select([
+        'user.id as id',
+        'user.email as email',
+        'user.role as role',
+        'user.createdAt as created_at',
+        'user.updatedAt as updated_at',
+        'user.deletedAt as deleted_at',
+      ])
+      .leftJoinAndSelect('user.userInfo', 'user_info')
+      .where('user.role = :role', { role: ROLE.USER })
+      .orderBy('user.createdAt', filterOptions.order)
+      .offset(skip)
+      .limit(filterOptions.limit);
+
+    return convertSnakeToCamel(await query.getRawMany());
   }
 }
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,12 @@
 import { SessionModule } from '@modules/session/session.module';
+import { AdminUserController } from '@modules/user/controller/admin-user.controller';
 import { UserInfoEntity } from '@modules/user/entities/user-info.entity';
 import { UserEntity } from '@modules/user/entities/user.entity';
 import { UserInfoRepository } from '@modules/user/repositories/user-info.repository';
 import { UserRepository } from '@modules/user/repositories/user.repository';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './user.controller';
+import { UserController } from './controller/user.controller';
 import { UserService } from './user.service';
 
 @Module({
@@ -13,7 +14,7 @@ import { UserService } from './user.service';
     TypeOrmModule.forFeature([UserEntity, UserInfoEntity]),
     SessionModule,
   ],
-  controllers: [UserController],
+  controllers: [UserController, AdminUserController],
   providers: [UserService, UserRepository, UserInfoRepository],
   exports: [UserService],
 })
