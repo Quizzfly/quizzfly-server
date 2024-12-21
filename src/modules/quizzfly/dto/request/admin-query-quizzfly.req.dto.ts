ea15123be20e58f80dac28a9a8c0c156e32b3e72
phduchuy <phanhoangduchuyy@gmail.com>
2024-12-21 19:00:43 +0700

    update: not require query param is_deleted

--- src/modules/quizzfly/dto/request/admin-query-quizzfly.req.dto.ts
+++ src/modules/quizzfly/dto/request/admin-query-quizzfly.req.dto.ts
@@ -1,10 +1,10 @@
 import { PageOptionsDto } from '@common/dto/offset-pagination/page-options.dto';
-import { BooleanField } from '@core/decorators/field.decorators';
+import { BooleanFieldOptional } from '@core/decorators/field.decorators';
 import { Expose } from 'class-transformer';
 
 @Expose()
 export class AdminQueryQuizzflyReqDto extends PageOptionsDto {
-  @BooleanField({ default: false, name: 'is_deleted' })
+  @BooleanFieldOptional({ default: false, name: 'is_deleted' })
   @Expose({ name: 'is_deleted' })
   readonly isDeleted?: boolean;
 }
