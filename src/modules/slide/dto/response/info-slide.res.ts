09c50abac90ab580e7fef47d4edfb3d1cd275fb9
LeTatTuan <letattuan04052003@gmail.com>
2025-01-02 12:47:55 +0700

    hotfix: update response for api create slide

--- src/modules/slide/dto/response/info-slide.res.ts
+++ src/modules/slide/dto/response/info-slide.res.ts
@@ -7,25 +7,31 @@ import {
   UUIDFieldOptional,
 } from '@core/decorators/field.decorators';
 import { FileResDto } from '@modules/file/dto/file.res.dto';
-import { Exclude } from 'class-transformer';
+import { Expose } from 'class-transformer';
 
-@Exclude()
+@Expose()
 export class InfoSlideResDto {
   @UUIDField()
+  @Expose()
   id: Uuid;
 
   @StringField()
+  @Expose()
   content: string;
 
   @ClassFieldOptional(() => FileResDto, { each: true, isArray: true })
+  @Expose()
   files?: FileResDto[];
 
   @StringField({ name: 'background_url' })
+  @Expose()
   backgroundUrl: string;
 
   @UUIDFieldOptional({ name: 'prev_element_id', nullable: true })
+  @Expose()
   prevElementId: Uuid = null;
 
   @ClassField(() => Date, { name: 'created_at' })
+  @Expose()
   createdAt: Date;
 }
